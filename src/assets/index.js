f11fc8f67d72ee6e84b2efdf83c31f8456e3102b
kanyecat <89665006+kanyecat@users.noreply.github.com>
2023-07-27 18:33:14 -0700

    clean up

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,26 +1,11 @@
-import bill from "./bill.png";
-import binance from "./binance.png";
-import card from "./card.png";
-import coinbase from "./coinbase.png";
-import dropbox from "./dropbox.png";
-import quotes from "./quotes.svg";
-import robot from "./robot.png";
 import send from "./Send.svg";
-import shield from "./Shield.svg";
-import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import google from "./google.svg";
-import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
 import calligraphy from "./calligraphy.png";
 import krtr_pot_v1 from "./krtr_pot_v1.png";
 import defenseMechanism from "./defenseMechanism.png";
@@ -60,29 +45,14 @@ import logo from "./logo.svg";
 export {
   me,
   logo,
-  bill,
-  binance,
-  card,
-  coinbase,
-  dropbox,
-  quotes,
-  robot,
   send,
-  shield,
-  star,
   menu,
   close,
   google,
-  apple,
   arrowUp,
-  discount,
   facebook,
   instagram,
   linkedin,
-  twitter,
-  people01,
-  people02,
-  people03,
   calligraphy,
   krtr_pot_v1,
   defenseMechanism,
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,7 @@
 import { instagram, calligraphy, mantis_enclosure, 
   recordplayer, krtr_pot_v1, krtr_pot_lantern, long_calli_postcard, 
   untold_story, defenseMechanism, notebookcoverdesign, single_propagation_holder, 
-  triple_propagation_holder, fortyDaysNights, linkedin, twitter, binance, 
-  coinbase, dropbox, wahlbergii, torso, sofa, relationships, seoul, corals,
+  triple_propagation_holder, fortyDaysNights, linkedin, wahlbergii, torso, sofa, relationships, seoul, corals,
   waterIntoWine, css, html, reactjs, tailwind, figma, git, javascript, typescript,
   nodejs, threejs, solidworks, arduino } from "../assets";
 
@@ -129,33 +128,6 @@ export const experiments = [
   },
 ];
 
-// export const feedback = [
-//   {
-//     id: "feedback-1",
-//     content:
-//       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-//     name: "Herman Jensen",
-//     title: "Founder & Leader",
-//     img: people01,
-//   },
-//   {
-//     id: "feedback-2",
-//     content:
-//       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-//     name: "Steve Mark",
-//     title: "Founder & Leader",
-//     img: people02,
-//   },
-//   {
-//     id: "feedback-3",
-//     content:
-//       "It is usually people in the money business, finance, and international trade that are really rich.",
-//     name: "Kenn Gallagher",
-//     title: "Founder & Leader",
-//     img: people03,
-//   },
-// ];
-
 export const stats = [
   {
     id: "stats-1",
@@ -225,19 +197,6 @@ export const footerLinks = [
       },
     ],
   },
-  {
-    title: "Partner",
-    links: [
-      {
-        name: "Our Partner",
-        link: "https://www.hoobank.com/our-partner/",
-      },
-      {
-        name: "Become a Partner",
-        link: "https://www.hoobank.com/become-a-partner/",
-      },
-    ],
-  },
 ];
 
 export const socialMedia = [
@@ -246,16 +205,6 @@ export const socialMedia = [
     icon: instagram,
     link: "https://www.instagram.com/jooheekim.art",
   },
-  // {
-  //   id: "social-media-2",
-  //   icon: facebook,
-  //   link: "https://www.facebook.com/",
-  // },
-  // {
-  //   id: "social-media-3",
-  //   icon: twitter,
-  //   link: "https://www.twitter.com/",
-  // },
   {
     id: "social-media-3",
     icon: linkedin,
@@ -311,20 +260,5 @@ export const technologies = [
   {
     name: "figma",
     icon: figma,
-  },
-];
-
-export const clients = [
-  {
-    id: "client-2",
-    logo: binance,
-  },
-  {
-    id: "client-3",
-    logo: coinbase,
-  },
-  {
-    id: "client-4",
-    logo: dropbox,
   },
 ];
