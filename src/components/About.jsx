c4a2827ac1a55d4a6bbc0f98f61b1d46afad211a
kanyecat <89665006+kanyecat@users.noreply.github.com>
2023-08-01 17:06:53 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,20 +9,25 @@ const About = () => {
   return (
     <>
     <div className={`bg-orangered-gradient ${styles.paddingX} ${styles.paddingY} ${styles.flexStart}`}>
-      <section id='home' className={`flex md:flex-row flex-col`}>
+      <section id='home' className={`flex flex-col`}>
         <div className='pb-10 md:pr-6'>
-          <span className='text-white font-arimo font-normal md:text-[18px] text-[16px]'>Hello, I'm Joo Hee. I am currently in the</span>
-          <span className='text-white font-grandiflora font-normal md:text-[22px] text-[17px]'> Bachelor of Engineering Studies and Arts (BESA) </span>
-          <span className='text-white font-arimo font-normal md:text-[18px] text-[16px]' >intercollege degree program at Carnegie Mellon University. I am pursuing
+          <span className='text-white font-arimo font-normal md:text-[24px] text-[16px]'>Hello, I'm Joo Hee. I am currently in the</span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> Bachelor of Engineering Studies and Arts (BESA) </span>
+          <span className='text-white font-arimo font-normal md:text-[24px] text-[16px]' >intercollege degree program at Carnegie Mellon University. I am pursuing
            two majors in the</span>
-          <span className='text-white font-grandiflora font-normal md:text-[22px] text-[17px]'> School of Art </span>
-          <span className='text-white font-arimo font-normal md:text-[18px] text-[16px]'>and</span>
-          <span className='text-white font-grandiflora font-normal md:text-[22px] text-[17px]'> Mechanical Engineering </span>
-          <span className='text-white font-arimo font-normal md:text-[18px] text-[16px]'>as well as a minor in</span>
-          <span className='text-white font-grandiflora font-normal md:text-[22px] text-[17px]'> Physical Computing (IDeATe)</span>
-          <span className='text-white font-arimo font-normal md:text-[18px] text-[16px]'>.</span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> School of Art </span>
+          <span className='text-white font-arimo font-normal md:text-[24px] text-[16px]'>and</span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> Mechanical Engineering </span>
+          <span className='text-white font-arimo font-normal md:text-[24px] text-[16px]'>as well as a minor in</span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> Physical Computing (IDeATe)</span>
+          <span className='text-white font-arimo font-normal md:text-[24px] text-[16px]'>.</span>
         </div>
-        <img src={me} className='w-[400px]'/>
+        <div className='flex md:flex-row flex-col justify-center items-center'>
+          <img src={me} className='w-[400px]' loading='lazy'/>
+          <div className='md:pl-6 justify-center items-center'>
+            <span className='text-white font-arimo font-normal md:text-[18px] text-[16px]'></span>
+          </div>
+        </div>
       </section>
     </div>
     <div className={`${styles.boxWidth}`}>
