60e7ada46019377da55dd2ba6071c684cbf6ec8c
kanyecat <89665006+kanyecat@users.noreply.github.com>
2023-08-01 17:12:19 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,14 +12,14 @@ const About = () => {
       <section id='home' className={`flex flex-col`}>
         <div className='pb-10 md:pr-6'>
           <span className='text-dimWhite font-arimo font-normal md:text-[24px] text-[16px]'>Hello, I'm Joo Hee. I am currently in the</span>
-          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> Bachelor of Engineering Studies and Arts (BESA) </span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[18px]'> Bachelor of Engineering Studies and Arts (BESA) </span>
           <span className='text-dimWhite font-arimo font-normal md:text-[24px] text-[16px]' >intercollege degree program at Carnegie Mellon University. I am pursuing
            two majors in the</span>
-          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> School of Art </span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[18px]'> School of Art </span>
           <span className='text-dimWhite font-arimo font-normal md:text-[24px] text-[16px]'>and</span>
-          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> Mechanical Engineering </span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[18px]'> Mechanical Engineering </span>
           <span className='text-dimWhite font-arimo font-normal md:text-[24px] text-[16px]'>as well as a minor in</span>
-          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[17px]'> Physical Computing (IDeATe)</span>
+          <span className='text-white font-grandiflora font-normal md:text-[26px] text-[18px]'> Physical Computing (IDeATe)</span>
           <span className='text-dimWhite font-arimo font-normal md:text-[24px] text-[16px]'>.</span>
         </div>
         <div className='flex md:flex-row flex-col justify-center items-center'>
