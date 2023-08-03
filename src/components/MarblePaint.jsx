bbb7bf9aca58b3ec4be87af2d7fa82ab3f8a1c62
kanyecat <89665006+kanyecat@users.noreply.github.com>
2023-08-02 17:53:24 -0700

    Update MarblePaint.jsx

--- src/components/MarblePaint.jsx
+++ src/components/MarblePaint.jsx
@@ -7,9 +7,14 @@ const MarblePaint = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`bg-primary bg-cover bg-no-repeat bg-center ${styles.paddingY} ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
+        <div className={`${styles.boxWidth} justify-center`}>
+          <div className='w-full px-6 text-center md:mb-4 mb-3'>
+            <span className='md:text-[90px] text-[50px] text-white font-grandiflora'>MARBLE PAINT</span>
+          </div>
           <img src={marblepaint2} className='w-full md:h-[400px] h-[100px] object-cover'/>
-          <span className='text-white'>coming soon</span>
+          <span className='text-white font-animo'>Marble Paint is a machine that allows you to paint with marbles using 
+          canvas movement. The whole machine consists of a canvas connected to four crank motion mechanisms moving 
+          in succession.</span>
         </div>
       </div>
     </div>
